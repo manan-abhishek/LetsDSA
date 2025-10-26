9e54ddb7000efcbd5f7d6d2f1a5de3177446ec1e
Abhishek Kumar Nayak <kumarabhi829121@gmail.com>
2025-10-26 15:47:17 +0530

    reversearray

--- Find_smallest.cpp
+++ Find_smallest.cpp
@@ -7,13 +7,15 @@ int main(){
     int nums[] = {5,44,1,7,12,-6};
     int size = 6;
     int smallest = INT_MAX;
+    int largest = INT_MIN;
 
     for(int i = 0; i<size; i++){
-        if(nums[i] < smallest){  //smallest = min(nums[i] , smallest);
-            smallest = nums[i];
+                                      //smallest = min(nums[i] , smallest);
+            smallest = min(nums[i], smallest);
+            largest = max(nums[i], largest);
         }
 
     }
-    cout<<smallest<<endl;
+    cout<<"smallest :"<<smallest<<endl;
     return 0;
 }
